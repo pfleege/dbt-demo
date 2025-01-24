4776c42cf60bf9d67cce0ff902cbbaf76c37b203
peter.fleege@gmail.com <peter.fleege@gmail.com>
2025-01-24 09:10:15 +0000

    ADD source freshness check to  loaded files

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -3,6 +3,7 @@ SELECT
     customer AS customer_id,
     ordered_at AS order_date,
     store_id,
-    order_total
+    order_total,
+    _etl_loaded_at
 
 FROM {{ source('jaffle_shop', 'orders') }}
